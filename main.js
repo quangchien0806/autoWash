b66bc34af701f9ad1150c83d134290e28bad1479
Your Name <you@example.com>
2021-10-12 17:18:03 -0700

    menuJS

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 var menuList = document.getElementById("menuList");
 var iconMenu = document.getElementById("icon_menu");
 iconMenu.onclick = function () {
-  if (menuList.style.display == "none") {
-    menuList.style.display = "block";
-  } else {
-    menuList.style.display = "none";
-  }
+  menuList.classList.toggle("dropdown");
 };
